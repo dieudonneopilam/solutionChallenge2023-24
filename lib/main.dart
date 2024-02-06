b4fba4d85bbb283fe042c39ab5adbcc4c3923f1b
dieudonne <dieudonneopilam2@gmail.com>
2024-02-06 04:41:54 -0500

    after integrate google_maps to android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:solutionchallenge2024/routes/route.dart';
+import 'package:yjy_google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
 void main() {
   runApp(const MyApp());
+  // Require Hybrid Composition mode on Android.
+  final GoogleMapsFlutterPlatform mapsImplementation =
+      GoogleMapsFlutterPlatform.instance;
+  if (mapsImplementation is GoogleMapsFlutterAndroid) {
+    // Force Hybrid Composition mode.
+   // mapsImplementation.useAndroidViewSurface = true;
+  }
+  // ···
 }
 
 class MyApp extends StatelessWidget {
--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -2,7 +2,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:solutionchallenge2024/logic/bloc/bottom_bloc.dart';
 import 'package:solutionchallenge2024/map.dart';
-import 'package:solutionchallenge2024/screens/bottom.dart';
 import 'package:solutionchallenge2024/screens/detail.post.dart';
 
 class RouteAppConfig {
@@ -14,7 +13,7 @@ class RouteAppConfig {
           name: 'home',
           builder: (context, state) => BlocProvider(
                 create: (context) => BottomBloc(),
-                child: MapSample(),
+                child: const MapSample(),
               ),
           routes: [
             GoRoute(
